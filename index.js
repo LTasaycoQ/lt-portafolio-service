7257149b3efda4caaf5581c4a954b8cd8a31b5f6
Luis Tasayco <luistasayco3030@gmail.com>
2026-04-28 20:19:44 +0000

    actualizacoin de correo envio xd

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const EMAIL_USER = "ltasayco769@gmail.com";
+const EMAIL_PASS = "uukz bgne ojya busv";
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: EMAIL_USER,
+        pass: EMAIL_PASS,
+    }
+});
+
+app.post('/evaluacion', async (req, res) => {
+    const {
+        name, email
+    } = req.body;
+
+    try {
+        await transporter.sendMail({
+            from: `"Portafolio WEB - " <${EMAIL_USER}>`,
+            to: "luistasayco3030@gmail.com",
+            subject: `Mensaje de Pagina web - ${name}`,
+            html: `
+                   hola ${name}
+
+            `,
+
+        });
+
+        res.json({ mensaje: '¡Correo enviado con éxito!' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Error interno en el servidor' });
+    }
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
